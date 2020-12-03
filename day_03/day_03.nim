9bf2bf221a599a27bc55368be865f9694bfd27fb
pintergreg <pintergreg@gmail.com>
2020-12-03 12:20:47 +0100

    Fix and simplify day 3 solution

--- day_03/day_03.nim
+++ day_03/day_03.nim
@@ -2,15 +2,10 @@ var trees = 0
 var pos = -3
 for line in lines "input":
     let width = len(line)
-    for i in countup(pos, width-1, 3):
-        if pos < 0:
-            pos = 0
-            break
-        echo line[i]
-        if line[i] == '#':
-            trees += 1
-        pos += 1 
-        if pos >= width:
-            pos = pos-width
-
+    pos += 3
+    if pos >= width:
+        pos = pos-width
+    echo line[pos]
+    if line[pos] == '#':
+        trees += 1
 echo trees
