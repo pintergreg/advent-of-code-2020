aafd2bc8ed9ba0a46dfd94a5fbc891314a752bdb
pintergreg <pintergreg@gmail.com>
2020-12-03 16:00:54 +0100

    Add solution for part 2

--- day_02/day_02.nim
+++ day_02/day_02.nim
@@ -1,10 +1,15 @@
 import strutils
 import re
 var correct = int(0)
+var correct_b = 0
 for line in lines "input":
     if line =~ re"(\d+)-(\d+)\ ([a-z]):\ (\w+)": # matches a key=value pair:
         # var policy = $(parseInt(matches[0]), parseInt(matches[1]), matches[2], matches[3])
         if count(matches[3], matches[2]) >= parseInt(matches[0]) and count(matches[3], matches[2]) <= parseInt(matches[1]):
             correct += 1
+        let pass = matches[3]
+        if pass[parseInt(matches[0])-1] == matches[2][0] xor pass[parseInt(matches[1])-1] == matches[2][0]:
+            correct_b += 1
 
 echo correct
+echo correct_b
