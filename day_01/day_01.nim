5b826d86be007e13eb963830b769f130642b33e6
pintergreg <pintergreg@gmail.com>
2020-12-03 15:39:21 +0100

    Add ugly solution for day 1 / part 2

--- day_01/day_01.nim
+++ day_01/day_01.nim
@@ -10,3 +10,11 @@ block outer:
             if i+j == 2020:
                 echo i*j
                 break outer
+
+block outer:
+    for i in list:
+        for j in list:
+            for k in list:
+                if i+j+k == 2020:
+                    echo i*j*k
+                    break outer
