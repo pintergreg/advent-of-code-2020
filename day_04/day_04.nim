39521725057c76c8d3ef98f1e806ad00dc1a0d7a
pintergreg <pintergreg@gmail.com>
2020-12-04 09:10:03 +0100

    Cleanup

--- day_04/day_04.nim
+++ day_04/day_04.nim
@@ -1,18 +1,18 @@
 import strutils
 import sets
 
-const passport_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]#, "cid"]
+const passport_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 var valid = 0
 var invalid = 0
 var missing = toHashSet(passport_fields)
-var e = 0
+var empty = 0
 for line in readFile("input").split("\n"):
     for field in line.strip.split(" "):
         missing.excl(field.split(":")[0])
 
-    if line.strip == "":  # record ended
-        e += 1
-        if len(missing) == 0:# or missing == toHashSet(["cid"]):
+    if line.strip == "":
+        empty += 1
+        if len(missing) == 0:
             valid += 1
         else:
             invalid += 1
@@ -20,4 +20,4 @@ for line in readFile("input").split("\n"):
 
 echo valid
 echo invalid
-echo e
+echo empty
