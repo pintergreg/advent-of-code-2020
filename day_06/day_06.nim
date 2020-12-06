f9307403e6d4512980527dfd13fe7058ed25773e
pintergreg <pintergreg@gmail.com>
2020-12-06 09:01:21 +0100

    Add day 6 part 2

--- day_06/day_06.nim
+++ day_06/day_06.nim
@@ -11,3 +11,16 @@ for line in readFile("input").split("\n"):
     for i in line:
         group_answers.incl(i)
 echo count
+
+var group_answers2 = newSeq[string](0)
+var count2 = 0
+for line in readFile("input").split("\n"):
+    if line.strip == "":
+        var temp = toHashset(group_answers2[0])
+        for i in group_answers2[1..^1]:
+            temp = temp * toHashSet(i)
+        count2 += len(temp)
+        group_answers2 = newSeq[string](0)
+        continue
+    group_answers2.add(line)
+echo count2
