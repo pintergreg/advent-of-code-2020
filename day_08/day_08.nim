46e868d5f7a21092065cc383e6e106870a8b48c0
pintergreg <pintergreg@gmail.com>
2020-12-10 11:28:45 +0100

    Add day 8 part 1

---
+++ day_08/day_08.nim
@@ -0,0 +1,27 @@
+import strutils
+
+var program = newSeq[string](0)
+for line in lines "input":
+    program.add(line)
+
+var acc = 0
+var i = 0
+var executed = newSeq[int](0)
+while true:
+    if i notin executed:
+        executed.add(i)
+    else:
+        echo "AGAIN"
+        break
+    # echo program[i]
+    let instruction = program[i].split(" ")[0]
+    let parameter = parseInt(program[i].split(" ")[1])
+    if instruction == "nop":
+        i += 1
+    if instruction == "acc":
+        acc += parameter
+        i += 1
+    if instruction == "jmp":
+        i += parameter
+    # echo acc
+echo acc
