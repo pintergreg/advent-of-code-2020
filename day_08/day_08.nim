51475fe1b0e472a9768b7645289758366b6b376b
pintergreg <pintergreg@gmail.com>
2020-12-10 11:40:23 +0100

    Make the execution testable
    The method returns the accelerator and a bool indicating the termination

--- day_08/day_08.nim
+++ day_08/day_08.nim
@@ -4,26 +4,30 @@ var program = newSeq[string](0)
 for line in lines "input":
     program.add(line)
 
-var acc = 0
-var i = 0
-var executed = newSeq[int](0)
-while true:
-    if i notin executed:
-        executed.add(i)
-    else:
-        echo "AGAIN"
-        break
-    # echo program[i]
-    if i == len(program):
-        break
-    let instruction = program[i].split(" ")[0]
-    let parameter = parseInt(program[i].split(" ")[1])
-    if instruction == "nop":
-        i += 1
-    if instruction == "acc":
-        acc += parameter
-        i += 1
-    if instruction == "jmp":
-        i += parameter
-    # echo acc
-echo acc
+proc executeProgram(program: seq[string]): (int, bool) =
+    var terminated = true
+    var acc = 0
+    var i = 0
+    var executed = newSeq[int](0)
+    while true:
+        if i notin executed:
+            executed.add(i)
+        else:
+            terminated = false
+            break
+        # echo program[i]
+        if i == len(program):
+            break
+        let instruction = program[i].split(" ")[0]
+        let parameter = parseInt(program[i].split(" ")[1])
+        if instruction == "nop":
+            i += 1
+        if instruction == "acc":
+            acc += parameter
+            i += 1
+        if instruction == "jmp":
+            i += parameter
+        # echo acc
+    return (acc, terminated)
+
+echo executeProgram(program)
