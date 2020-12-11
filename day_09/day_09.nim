24fc75349e5bef1df04f06e5b9b6bd55428ee1bb
pintergreg <pintergreg@gmail.com>
2020-12-11 17:06:01 +0100

    Add day 9 part 2

--- day_09/day_09.nim
+++ day_09/day_09.nim
@@ -1,5 +1,6 @@
 import itertools
 import strutils
+import math
 
 var lines = newSeq[int](0)
 const window = 25
@@ -18,10 +19,19 @@ proc validate(slice: seq[int], target: int): bool =
                     break outer
     return
 
-
+var bad = 0
 for i in windowed(lines, window+1):
     # echo (i[0..^2], i[^1])
     # echo (i[0..^2], i[^1], validate(i[0..^2], i[^1]))
     if not validate(i[0..^2], i[^1]):
-        echo i[^1]
+        bad = i[^1]
         break
+
+echo bad
+for width in 4..20: # not really smart should find a more precise limit
+    for i in windowed(lines, width):
+        if sum(i[0..^1]) == bad:
+            let min = min(i[0..^2])
+            let max = max(i[0..^2])
+            echo min+max
+            break
